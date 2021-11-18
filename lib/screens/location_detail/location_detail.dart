1c661e625f19ba14f3a303d0b72b18d1837a238a
Peter Sun <pitaah@umich.edu>
2021-11-18 08:54:40 -0500

    - Added image (had to run `flutter clean` to fix image not showing up)

--- lib/screens/location_detail/location_detail.dart
+++ lib/screens/location_detail/location_detail.dart
@@ -1,6 +1,7 @@
 // screens/location_detail/location_detail.dart
 
 import 'package:flutter/material.dart';
+import 'image_banner.dart';
 import 'text_section.dart';
 
 class LocationDetail extends StatelessWidget {
@@ -14,6 +15,7 @@ class LocationDetail extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
+            ImageBanner("assets/images/kiyomizu-dera.jpg"),
             TextSection(Colors.red),
             TextSection(Colors.green),
             TextSection(Colors.blue),
